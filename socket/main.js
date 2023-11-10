0fc593e3133b5c0cb98d18c2308d6b223d0ce10a
sung eunjin <sunny@wisa.co.kr>
2023-11-10 16:54:20 +0900

    [node] socket 작업중

---
+++ socket/main.js
@@ -0,0 +1,16 @@
+const Koa = require('koa');
+const Pug = require('koa-pug');
+const path = require('path');
+
+const app = new Koa();
+
+new Pug({
+    viewPath: path.resolve(__dirname, './views'),
+    app,
+});
+
+app.use(async (ctx) => {
+    await ctx.render('main');
+});
+
+app.listen(3000);
